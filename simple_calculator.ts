eb99d9f287f322828f2622de8334df6623cc0741
faiza shoukat <165581200+faizashoukat@users.noreply.github.com>
2024-05-12 15:30:47 -0700

    Simple_calculator00

---
+++ simple_calculator.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+
+const answer = await inquirer.prompt([
+  { message: 'Enter first number :', type: 'number', name: 'FirstNumber' },
+  { message: 'Enter second number :', type: 'number', name: 'SecondNumber' },
+  {
+    message: 'Select one of the operators to perform operation',
+    type: 'List',
+    name: 'operator',
+    choices: ['Addition','Subtraction','Multiplication','Division'],
+  },
+]);
+
+//conditional Statements
+
+if(answer.operator==='Addition')
+{
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if(answer.operator==='Subtraction')
+{
+    console.log( answer.FirstNumber - answer.SecondNumber);
+}
+else if(answer.operator==='Multiplication')
+{
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if(answer.operator==='Division')
+{
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+    else
+{
+ 
+    console.log("please select Valid operator");
+}
